29777d4a09566981590ffbe2dce9025d96096796
Yurii <leskiv.yurii@gmail.com>
2024-08-14 16:44:40 +0300

    T-4 [Yurii Leskiv] Created security filter configuration

---
+++ user-service/src/main/java/org/arpha/config/FilterChainConfiguration.java
@@ -0,0 +1,36 @@
+package org.arpha.config;
+
+import org.arpha.security.jwt.AuthJwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+@EnableWebSecurity
+public class FilterChainConfiguration {
+
+    @Autowired
+    private AuthJwtTokenFilter authJWTTokenFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)// for local testing
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/login").permitAll()
+                        .requestMatchers(POST, "/api/v1/users").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(authJWTTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
