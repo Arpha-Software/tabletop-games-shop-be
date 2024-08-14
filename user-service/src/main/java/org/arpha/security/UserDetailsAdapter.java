9bc97bba6e89ebc47bb423cf94b880e2c9fde06e
Yurii <leskiv.yurii@gmail.com>
2024-08-14 16:48:02 +0300

    T-4 [Yurii Leskiv] Deleted lombok annotation

--- user-service/src/main/java/org/arpha/security/UserDetailsAdapter.java
+++ user-service/src/main/java/org/arpha/security/UserDetailsAdapter.java
@@ -1,6 +1,5 @@
 package org.arpha.security;
 
-import lombok.Getter;
 import org.arpha.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Getter
 public record UserDetailsAdapter(User user) implements UserDetails {
 
     @Override
