3b947e42bb647182155c77b62ca4c3d354144ad0
Yurii <leskiv.yurii@gmail.com>
2024-08-14 16:45:33 +0300

    T-4 [Yurii Leskiv] Implemented auth service

---
+++ user-service/src/main/java/org/arpha/service/AuthServiceImpl.java
@@ -0,0 +1,31 @@
+package org.arpha.service;
+
+import org.arpha.dto.user.request.LoginRequest;
+import org.arpha.dto.user.response.LoginResponse;
+import org.arpha.security.UserDetailsAdapter;
+import org.arpha.security.jwt.JwtUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest loginRequest) {
+        Authentication authenticated = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        UserDetailsAdapter user = (UserDetailsAdapter) authenticated.getPrincipal();
+        String jwtToken = jwtUtils.generateToken(user.getUsername());
+        return new LoginResponse(user.getUsername(), jwtToken);
+
+    }
+}
