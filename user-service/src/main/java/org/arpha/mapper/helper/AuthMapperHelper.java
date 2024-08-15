fe964c23353eceb327c425ee6e0bceed7f1653a6
Yurii <leskiv.yurii@gmail.com>
2024-08-15 18:22:21 +0300

    T-4 [Yurii Leskiv] Created auth mapper for login response

---
+++ user-service/src/main/java/org/arpha/mapper/helper/AuthMapperHelper.java
@@ -0,0 +1,21 @@
+package org.arpha.mapper.helper;
+
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.arpha.security.jwt.JwtUtils;
+import org.mapstruct.Named;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class AuthMapperHelper {
+
+    private final JwtUtils jwtUtils;
+
+    @Named("toExpirationDate")
+    public LocalDateTime getExpirationDate(String jwtToken) {
+        return jwtUtils.getExpirationDate(jwtToken);
+    }
+}
--- user-service/src/main/java/org/arpha/service/AuthServiceImpl.java
+++ user-service/src/main/java/org/arpha/service/AuthServiceImpl.java
@@ -3,12 +3,16 @@ package org.arpha.service;
 import lombok.RequiredArgsConstructor;
 import org.arpha.dto.user.request.LoginRequest;
 import org.arpha.dto.user.response.LoginResponse;
+import org.arpha.mapper.AuthMapper;
 import org.arpha.security.UserDetailsAdapter;
 import org.arpha.security.jwt.JwtUtils;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +20,17 @@ public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final AuthMapper authMapper;
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
-        Authentication authenticated = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-        UserDetailsAdapter user = (UserDetailsAdapter) authenticated.getPrincipal();
-        String jwtToken = jwtUtils.generateToken(user.getUsername());
-        return new LoginResponse(user.getUsername(), jwtToken);
-
+        return Optional
+                .of(loginRequest)
+                .map(loginRequest1 -> authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest1.getUsername(), loginRequest1.getPassword())))
+                .map(authentication -> (UserDetailsAdapter) authentication.getPrincipal())
+                .map(UserDetailsAdapter::user)
+                .map(user -> authMapper.toLoginResponse(user, jwtUtils.generateToken(user.getEmail())))
+                .orElseThrow(() -> new BadCredentialsException("Invalid username or password"));
     }
+
 }
