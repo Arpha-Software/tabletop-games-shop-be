b7167882c6bb4b393b761ffe9c81fd690bb4f139
Yurii <leskiv.yurii@gmail.com>
2024-08-15 15:52:08 +0300

    T-4 [Yurii Leskiv] Deleted pattern annotation on password field and added not empty

--- common/src/main/java/org/arpha/dto/user/request/LoginRequest.java
+++ common/src/main/java/org/arpha/dto/user/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package org.arpha.dto.user.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +14,7 @@ public class LoginRequest {
 
     @Email(message = "This email is not valid!")
     private String username;
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "Password must contain at least one number, lower case and upper case letters and be 8 symbols long!")
+    @NotEmpty(message = "Password can't be empty!")
     private String password;
 
 }
