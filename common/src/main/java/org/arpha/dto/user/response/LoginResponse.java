09dfd4b85a1234c66418bfbc35912c8ad72a4bfd
Yurii <leskiv.yurii@gmail.com>
2024-08-15 18:22:45 +0300

    T-4 [Yurii Leskiv] Changed LoginResponse to provide more information

--- common/src/main/java/org/arpha/dto/user/response/LoginResponse.java
+++ common/src/main/java/org/arpha/dto/user/response/LoginResponse.java
@@ -3,14 +3,15 @@ package org.arpha.dto.user.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.arpha.dto.user.TokenDetails;
+import org.arpha.dto.user.UserDetails;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginResponse {
 
-    private String username;
-    private String token;
-    //UserDetails firstName lastName email
-    //TokenDetails token, expires (LocalDateTime)
+    private TokenDetails tokenDetails;
+    private UserDetails userDetails;
+
 }
