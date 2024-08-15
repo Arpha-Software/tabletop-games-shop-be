6e1e7aee576f061631113eeec5e4be3f1b88bc65
Yurii <leskiv.yurii@gmail.com>
2024-08-15 19:40:41 +0300

    T-4 [Yurii Leskiv] Added response for password changing logic

---
+++ common/src/main/java/org/arpha/dto/user/response/ChangePasswordResponse.java
@@ -0,0 +1,15 @@
+package org.arpha.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordResponse {
+
+    private long id;
+    private boolean isPasswordChanged;
+
+}
