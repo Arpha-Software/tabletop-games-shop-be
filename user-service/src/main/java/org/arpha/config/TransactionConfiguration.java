0de09c6e01638c97681395dfba0cf910689a046c
Yurii <leskiv.yurii@gmail.com>
2024-08-17 20:40:56 +0300

    T-2 [Yurii Leskiv] Added transaction configuration

---
+++ user-service/src/main/java/org/arpha/config/TransactionConfiguration.java
@@ -0,0 +1,9 @@
+package org.arpha.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfiguration {
+}
