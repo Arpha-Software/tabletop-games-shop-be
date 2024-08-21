0e57bbc473ce459e022a00da1b7b2054d83e0dc9
Yurii <leskiv.yurii@gmail.com>
2024-08-21 15:54:53 +0300

    T-3 [Yurii Leskiv] Added repository for file entity

---
+++ media-service/src/main/java/org/arpha/repository/FileRepository.java
@@ -0,0 +1,10 @@
+package org.arpha.repository;
+
+import org.arpha.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+
+}
