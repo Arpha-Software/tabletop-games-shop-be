fb5ac782e340568012f9a6445e513b1a978b0550
Yurii <leskiv.yurii@gmail.com>
2024-08-21 16:01:05 +0300

    T-3 [Yurii Leskiv] Added handler for new type of exception when file isn't found

---
+++ media-service/src/main/java/org/arpha/exception/handle/MediaExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.arpha.exception.handle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.arpha.exception.FileNotFoundException;
+import org.arpha.exception.FileUploadException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class MediaExceptionHandler {
+
+    @ExceptionHandler(FileUploadException.class)
+    public ProblemDetail handleFileUploadException(FileUploadException e) {
+        log.error(StringUtils.EMPTY, e);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ProblemDetail handleFileNotFoundException(FileNotFoundException e) {
+        log.error(StringUtils.EMPTY, e);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+}
+
