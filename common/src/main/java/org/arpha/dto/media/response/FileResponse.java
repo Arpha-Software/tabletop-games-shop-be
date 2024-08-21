f269e293cd93fa07ba7a9b35121fea3ed4aef7dd
Yurii <leskiv.yurii@gmail.com>
2024-08-21 16:02:43 +0300

    T-3 [Yurii Leskiv] Added response for file uploading

---
+++ common/src/main/java/org/arpha/dto/media/response/FileResponse.java
@@ -0,0 +1,20 @@
+package org.arpha.dto.media.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.arpha.dto.media.enums.UploadType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileResponse {
+
+    private long id;
+    private String fileName;
+    private long size;
+    private long targetId;
+    private String fileType;
+    private UploadType targetType;
+
+}
