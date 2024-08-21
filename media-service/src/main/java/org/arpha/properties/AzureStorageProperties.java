e16211f6fd8f884a078ab49a01b842f942c494c1
Yurii <leskiv.yurii@gmail.com>
2024-08-21 15:51:42 +0300

    T-3 [Yurii Leskiv] Added configuration for azure storage

---
+++ media-service/src/main/java/org/arpha/properties/AzureStorageProperties.java
@@ -0,0 +1,9 @@
+package org.arpha.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(AzureStorageProperties.class)
+@ConfigurationProperties("azure.services.storage")
+public record AzureStorageProperties(String connectionString, String containerName) {
+}
