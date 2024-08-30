c9a8f4716913889d96ea66c5dafe71d1fc004e33
Yurii <leskiv.yurii@gmail.com>
2024-08-30 20:03:25 +0300

    T-2 [Yurii Leskiv] Added authorization for auth controller

--- user-service/src/main/java/org/arpha/controller/AuthController.java
+++ user-service/src/main/java/org/arpha/controller/AuthController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.arpha.dto.user.request.LoginRequest;
 import org.arpha.dto.user.response.LoginResponse;
 import org.arpha.service.AuthService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @PreAuthorize("isAnonymous()")
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid LoginRequest loginRequest) {
         return authService.login(loginRequest);
