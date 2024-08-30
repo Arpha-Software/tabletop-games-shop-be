4ea5ae1354aebe195ffeab6663750f60aeb1cd5a
Yurii <leskiv.yurii@gmail.com>
2024-08-30 20:04:27 +0300

    T-2 [Yurii Leskiv] Added config for method security annotations

---
+++ core/src/main/java/org/arpha/core/config/MethodSecurityConfiguration.java
@@ -0,0 +1,7 @@
+package org.arpha.core.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@EnableMethodSecurity
+public class MethodSecurityConfiguration {
+}
