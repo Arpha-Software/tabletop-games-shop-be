d2d41f164567af02375d05600a44c63cc76a16c5
Yurii <leskiv.yurii@gmail.com>
2024-09-12 20:26:31 +0300

    T-24 [Yurii Leskiv] Refactored LoginResponse

--- user-service/src/main/java/org/arpha/mapper/AuthMapper.java
+++ user-service/src/main/java/org/arpha/mapper/AuthMapper.java
@@ -1,7 +1,7 @@
 package org.arpha.mapper;
 
 import org.arpha.dto.user.response.LoginResponse;
-import org.arpha.entity.User;
+import org.arpha.dto.user.response.UserResponse;
 import org.arpha.mapper.helper.AuthMapperHelper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -11,10 +11,10 @@ public interface AuthMapper {
 
     @Mapping(target = "tokenDetails.token", source = "jwtToken")
     @Mapping(target = "tokenDetails.expires", source = "jwtToken", qualifiedByName = "toExpirationDate")
-    @Mapping(target = "userDetails.id", source = "user.id")
-    @Mapping(target = "userDetails.firstName", source = "user.firstName")
-    @Mapping(target = "userDetails.lastName", source = "user.lastName")
-    @Mapping(target = "userDetails.email", source = "user.email")
-    LoginResponse toLoginResponse(User user, String jwtToken);
+    @Mapping(target = "userDetails.firstName", source = "userResponse.firstName")
+    @Mapping(target = "userDetails.lastName", source = "userResponse.lastName")
+    @Mapping(target = "userDetails.email", source = "userResponse.email")
+    @Mapping(target = "userDetails.id", source = "userResponse.id")
+    LoginResponse toLoginResponse(UserResponse userResponse, String jwtToken);
 
 }
