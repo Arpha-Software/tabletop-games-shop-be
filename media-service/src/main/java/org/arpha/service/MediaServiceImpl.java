9f043c5f95bd4cc00d13da26c1ad93f75a2166cd
Yurii <leskiv.yurii@gmail.com>
2024-10-05 17:42:34 +0300

    T-8 [Yurii Leskiv] Switch from Aws to Azure storage

--- media-service/src/main/java/org/arpha/service/MediaServiceImpl.java
+++ media-service/src/main/java/org/arpha/service/MediaServiceImpl.java
@@ -1,15 +1,15 @@
 package org.arpha.service;
 
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
 import com.querydsl.core.types.Predicate;
 import lombok.RequiredArgsConstructor;
-import org.arpha.dto.media.enums.AccessType;
 import org.arpha.dto.media.enums.TargetType;
 import org.arpha.dto.media.request.FileUploadRequest;
 import org.arpha.dto.media.response.FileResponse;
 import org.arpha.entity.File;
 import org.arpha.exception.FileNotFoundException;
 import org.arpha.exception.FileUploadException;
-import org.arpha.exception.GenerateFileLinkException;
 import org.arpha.mapper.FileMapper;
 import org.arpha.repository.FileRepository;
 import org.arpha.utils.Boxed;
@@ -17,53 +17,40 @@ import org.arpha.validator.FileRequestValidator;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.MimeType;
-import software.amazon.awssdk.awscore.presigner.PresignedRequest;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
-import java.net.URL;
 import java.util.UUID;
+
+import static org.arpha.dto.media.enums.AccessType.READ;
+import static org.arpha.dto.media.enums.AccessType.WRITE;
+
 
 @Service
 @RequiredArgsConstructor
 public class MediaServiceImpl implements MediaService {
 
-    private final AwsRequestService awsRequestService;
+    private final BlobContainerClient blobContainerClient;
     private final FileRequestValidator fileRequestValidator;
     private final FileRepository fileRepository;
     private final FileMapper fileMapper;
-    private final S3Presigner s3Presigner;
-    private final S3Client s3client;
 
     @Override
-    @Transactional
     public FileResponse upload(FileUploadRequest fileUploadRequest) {
         return Boxed
                 .of(fileUploadRequest)
-                .filter(fileRequestValidator::validate)
-                .mapToBoxed(fileName1 -> fileMapper.toFile(fileUploadRequest, generateFileName(fileUploadRequest)))
+                .mapToBoxed(this::generateFileName)
+                .mapToBoxed(blobContainerClient::getBlobClient)
+                .filter(blobClient -> fileRequestValidator.validate(fileUploadRequest, blobClient))
+                .mapToBoxed(blobClient -> fileMapper.toFile(fileUploadRequest, blobClient))
                 .mapToBoxed(fileRepository::save)
-                .mapToBoxed(file -> fileMapper.toFileResponse(file, AccessType.WRITE,
-                        awsRequestService.sendS3Request(file).url().toExternalForm()))
+                .mapToBoxed(file -> fileMapper.toFileResponse(file, WRITE))
                 .orElseThrow(() -> new FileUploadException("File wasn't uploaded. Either entity with target id doesn't" +
                                                            " exist!"));
     }
 
     @Override
     public Page<FileResponse> getAll(Predicate predicate, Pageable pageable) {
-        return fileRepository.findAll(predicate, pageable).map(file -> fileMapper.toFileResponse(file, AccessType.READ, getReadLink(file)));
-    }
-
-    private String getReadLink(File file) {
-        return Boxed
-                .of(file)
-                .mapToBoxed(awsRequestService::getObjectPresignRequest)
-                .mapToBoxed(s3Presigner::presignGetObject)
-                .mapToBoxed(PresignedRequest::url)
-                .mapToBoxed(URL::toExternalForm)
-                .orElseThrow(() -> new GenerateFileLinkException("Couldn't create link for %s file".formatted(file.getName())));
+        return fileRepository.findAll(predicate, pageable).map(file -> fileMapper.toFileResponse(file, READ));
     }
 
     @Override
@@ -71,9 +58,10 @@ public class MediaServiceImpl implements MediaService {
         Boxed
                 .of(id)
                 .flatOpt(fileRepository::findById)
-                .mapToBoxed(awsRequestService::getDeleteObjectRequest)
-                .mapToBoxed(s3client::deleteObject)
-                .ifPresent(file -> fileRepository.deleteById(id));
+                .mapToBoxed(File::getName)
+                .mapToBoxed(blobContainerClient::getBlobClient)
+                .mapToBoxed(BlobClient::deleteIfExists)
+                .ifPresent(result -> fileRepository.deleteById(id));
     }
 
     @Override
@@ -81,8 +69,7 @@ public class MediaServiceImpl implements MediaService {
         return Boxed
                 .of(id)
                 .flatOpt(fileRepository::findById)
-                .mapToBoxed(file -> fileMapper.toFileResponse(file, AccessType.READ, s3Presigner.presignGetObject(
-                        awsRequestService.getObjectPresignRequest(file)).url().toExternalForm()))
+                .mapToBoxed(file -> fileMapper.toFileResponse(file, READ))
                 .orElseThrow(() -> new FileNotFoundException("File with %s id wasn't found!".formatted(id)));
     }
 
