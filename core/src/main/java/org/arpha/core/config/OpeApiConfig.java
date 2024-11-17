0123173f754f601258dd650d610f1ed0e4f0730e
Yurii <leskiv.yurii@gmail.com>
2024-11-17 20:09:27 +0200

    [Yurii Leskiv] Added servers for swagger

--- core/src/main/java/org/arpha/core/config/OpeApiConfig.java
+++ core/src/main/java/org/arpha/core/config/OpeApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,7 +13,10 @@ import org.springframework.context.annotation.Configuration;
                 title = "Tabletop-games-shop",
                 version = "1.0",
                 description = "API"
-        )
+        ),
+        servers = {@Server(url = "/"), @Server(url = "https://tgs-test-aab09ba6a40b.herokuapp.com"),
+                @Server(url = "https://tgs-test-aab09ba6a40b.herokuapp.com/swagger-ui/index.html"),
+                @Server(url = "http://localhost:3000")}
 )
 @SecurityScheme(
         name = "Bearer Authentication",
