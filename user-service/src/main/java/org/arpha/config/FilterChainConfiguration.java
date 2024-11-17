c913c57787a5a161dd03a3f5e280a4fc6b34799e
Yurii <leskiv.yurii@gmail.com>
2024-11-17 20:30:20 +0200

    [Yurii Leskiv] disabled cors for test

--- user-service/src/main/java/org/arpha/config/FilterChainConfiguration.java
+++ user-service/src/main/java/org/arpha/config/FilterChainConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -35,6 +36,7 @@ public class FilterChainConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(CorsConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)// for local testing
                 .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
