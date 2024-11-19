9ee0f0b5e48bcb05c5ae319caa12869c3b406d07
Yurii <leskiv.yurii@gmail.com>
2024-11-19 02:16:33 +0200

    [Yurii Leskiv] Added properties for orders

---
+++ order-service/src/main/java/org/arpha/property/NovaPoshtaConsignmentProperties.java
@@ -0,0 +1,9 @@
+package org.arpha.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@ConfigurationProperties("nova-poshta.config")
+@EnableConfigurationProperties(NovaPoshtaConsignmentProperties.class)
+public record NovaPoshtaConsignmentProperties(String apiUrl, String senderPhone, String senderIdentification) {
+}
