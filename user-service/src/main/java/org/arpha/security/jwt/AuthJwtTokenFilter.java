8209b15c3ea29ed7d24eebad2e62d585bd9f2585
OleksiiDatsiuk <Olmine2013@gmail.com>
2025-05-12 00:19:47 +0300

    [Oleksii Datsiuk] Fixed 503 error

--- user-service/src/main/java/org/arpha/security/jwt/AuthJwtTokenFilter.java
+++ user-service/src/main/java/org/arpha/security/jwt/AuthJwtTokenFilter.java
@@ -38,6 +38,13 @@ public class AuthJwtTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         if (!hasAuthorizationBearer(request)) {
             sendErrorResponse(response, "Authorization token is missing!");
         }
