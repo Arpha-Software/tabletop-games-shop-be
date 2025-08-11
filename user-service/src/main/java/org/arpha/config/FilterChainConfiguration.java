cef992f08cff09706d4168fbb4fe156c5fc7af44
OleksiiDatsiuk <Olmine2013@gmail.com>
2025-08-12 00:47:35 +0300

    [Oleksii Datsiuk] Improved filtering

--- user-service/src/main/java/org/arpha/config/FilterChainConfiguration.java
+++ user-service/src/main/java/org/arpha/config/FilterChainConfiguration.java
@@ -52,6 +52,8 @@ public class FilterChainConfiguration {
                         .requestMatchers(GET, "/api/v1/products/{id}").permitAll()
                         .requestMatchers(POST, "/api/v1/products/filter").permitAll()
                         .requestMatchers(GET, "/api/v1/products/{id}/reviews").permitAll()
+                        .requestMatchers(GET, "/api/v1/blog/posts/{id}").permitAll()
+                        .requestMatchers(GET, "/api/v1/blog/posts").permitAll()
                         .requestMatchers(GET, "/api/v1/wishlists/share/{shareableLink}").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
